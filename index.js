3ed293152b83e27f690e4079396e12b246fff340
oshiro-kazuki <68804296+oshiro-kazuki@users.noreply.github.com>
2020-11-13 12:36:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 'use strict';
 
 {
+  //メインイメージ
   const topImg = document.getElementById('topImg');
   const topImage = document.createElement('img');
   topImage.src = "images/top.JPG";
   topImg.appendChild(topImage);
   
   
+  //コンテンツのドリンクイメージのスライド
   const drinkImg = ["images/drink01.JPG", "images/drink02.JPG", "images/drink03.JPG"];
-        
+  let index = 0;
+  
+  function changeImage() {
+    const img = document.querySelector('.content > img');
+    if(index === drinkImg.length - 1) {
+      index = 0;
+    } else {
+      index++
+    }
+    img.src = drinkImg[index];
+  }
+
+  setInterval(changeImage, 1000);
+  
+  
+ //コンテンツ
   const menus = [
-    {title: "Drink", image: drinkImg[0], html: "drink.html"},
+    {title: "Drink", image: drinkImg[index], html: "drink.html"},
     {title: "Food", image: "images/food01.JPG", html: "food.html"},
     {title: "Takeout", image: "images/takeout00.JPG", html: "takeout.html"},
     {title: "Party", image: "images/floor.JPG", html: "party.html"}
   ];
 
+  
+  //コンテンツのdom生成
   const contents = document.getElementById('contents');
-
+  
   for(let i = 0; i < menus.length; i++) {
     const a = document.createElement('a');
     a.href = menus[i].html;
@@ -32,47 +51,7 @@
     contents.appendChild(a);
   }
 
-
-
-  let corrent = 0;
-  const contentImage = document.querySelector('.content > img');
-  contentImage.src = drinkImg[corrent];
-
-  drinkImg.forEach((image, index) => {
-    const img = document.createElement('img');
-    img.src = image;
-    img.classList.add('erase');
-    
-    const span = document.createElement('span');
-    span.classList.add('erase');
-
-    span.addEventListener('click', () => {
-      contentImage.src = image;
-      corrent = index;
-    });
-
-    span.appendChild(img);
-    contents.appendChild(span);
-  });
-
-  const span = document.createElement('span');
-  span.classList.add('erase');
-
-  span.addEventListener('click', () => {
-    let target = corrent + 1;
-    if(drinkImg.length === target) {
-      target = 0;
-    }
-    document.querySelectorAll('#contents > span')[target].click();
-  });
-
-  setInterval(() => {
-    span.click();
-  }, 2000)
-  
-
-
-
+  //コンテンツのスクロール時の表示
   function contentShow() {
     const content = document.querySelectorAll('.content');
     
@@ -91,7 +70,7 @@
   window.addEventListener('scroll', contentShow);
 
 
-
+  //紹介文ボタン生成
   const mask = document.getElementById('mask');
   const aboutBtn = document.getElementById('aboutBtn');
   const about = document.getElementById('about');
@@ -100,12 +79,14 @@
   const aboutImg = document.querySelector('#about > img');
   aboutImg.src = "images/yagi.JPG";
 
+  //紹介ボタンのクリック表示
   aboutBtn.addEventListener('click', () => {
     aboutBtn.classList.add('hidden');
     about.classList.remove('hidden');
     mask.classList.remove('hidden');
   });
 
+  //紹介ボタンのクリック非表示
   mask.addEventListener('click', () => {
     mask.classList.add('hidden');
     about.classList.add('hidden');
@@ -116,7 +97,7 @@
   });
 
 
-
+  //紹介ボタンのスクロール非表示
   function aboutBtnClose() {
     const header = document.querySelector('header');
     const scrollY = window.pageYOffset;
